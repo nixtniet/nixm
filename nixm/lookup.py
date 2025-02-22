46e2759501a0367258815330f587de8fda758f1d
nixt <nixtniet@gmail.com>
2025-02-22 20:28:48 +0100

    add lookup table

---
+++ nixm/lookup.py
@@ -0,0 +1,40 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "ask": "nixm.mods.llm",
+    "cfg": "nixm.mods.irc",
+    "cor": "nixm.mods.mbx",
+    "dis": "nixm.mods.mdl",
+    "dne": "nixm.mods.tdo",
+    "dpl": "nixm.mods.rss",
+    "eml": "nixm.mods.mbx",
+    "err": "nixm.mods.err",
+    "exp": "nixm.mods.rss",
+    "flt": "nixm.mods.flt",
+    "fnd": "nixm.mods.fnd",
+    "imp": "nixm.mods.rss",
+    "lne": "nixm.mods.slg",
+    "log": "nixm.mods.log",
+    "mbx": "nixm.mods.mbx",
+    "mod": "nixm.mods.mod",
+    "mre": "nixm.mods.irc",
+    "nme": "nixm.mods.rss",
+    "now": "nixm.mods.mdl",
+    "pwd": "nixm.mods.irc",
+    "rem": "nixm.mods.rss",
+    "req": "nixm.mods.req",
+    "res": "nixm.mods.rss",
+    "rss": "nixm.mods.rss",
+    "slg": "nixm.mods.slg",
+    "syn": "nixm.mods.rss",
+    "tdo": "nixm.mods.tdo",
+    "thr": "nixm.mods.thr",
+    "tmr": "nixm.mods.tmr",
+    "udp": "nixm.mods.udp",
+    "upt": "nixm.mods.upt",
+    "wsd": "nixm.mods.wsd"
+}
